7c9bf1991cb52c05e62da3f6b6bf62a1b4e12be3
ihgazni2 <terryinzaghi@163.com>
2019-10-02 05:25:47 +0000

    remove old build

--- efuntool/ebooltool.py
+++ efuntool/ebooltool.py
@@ -25,7 +25,7 @@ def is_zero_len(value):
 
 def is_fls(value,*args):
     if(len(args)==0 or args[0] == []):
-        rslt = True if(identity(value,0) or identity(value,False) or (value == None) or is_zero_len(value) ) else True
+        rslt = True if(identity(value,0) or identity(value,False) or (value == None) or is_zero_len(value) ) else False
         return(rslt)
     else:
         flses = args[0]
